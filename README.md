f17dab63d3889dfa2faff550160d39d57b375540
julianofavero <julianozinho67@gmail.com>
2023-04-17 09:01:35 -0300

    segundo commit para subir arquivos para repositório guitHub

---
+++ README.md
@@ -0,0 +1,8 @@
+<<<<<<< HEAD
+# sa5
+repositorio para a sa5
+=======
+# testeGit
+Repositório para testes de commit para o github
+>>>>>>> ca12443aa80448d2df5baa1d3f9702ecf8195990
+
---
+++ mainBooks/src/main/java/br/com/example/exemplo_spring/servico/ILivroServico.java
@@ -0,0 +1,18 @@
+package br.com.example.exemplo_spring.servico;
+
+import java.util.List;
+import br.com.example.exemplo_spring.modelo.*;
+
+public interface ILivroServico {
+
+	public Livro salvarLivro(Livro livro);
+
+	public List<Livro> buscarTodosLivros();
+
+	public Livro buscarLivroPorId(Long id);
+
+	public void deletarLivroPorId(Long id);
+
+	public void atualizarLivro(Livro livro);
+
+}
